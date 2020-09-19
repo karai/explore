c9a0440c884719d04ae74c694444ba75d37da970
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-09-19 15:53:15 +0200

    switch channel on dropdown select

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,128 @@
-let channel = "http://167.71.104.172:4200";
+const channels = {
+  icarus: {
+    name: 'Icarus (Unstable)',
+    url: 'http://167.71.104.172:4200'
+  },
+  daedalus: {
+    name: 'Daedalus (Stable)',
+    url: 'http://46.101.200.220:4200'
+  }
+}
+
+let channel;
 let transactionsTable;
 
 $(document).ready(function() {
-  fetchChannelStats();
-  fetchTransactions();
+  initChannelSelector();
+  initTransactionsTable();
 
+  switchChannel(channels.icarus);
+  startRefreshTimer(20000);
+
+  $('#searchValue').keydown(function (e) {
+    // setSearchValueErrorState(false);
+
+    // check if 'Enter' key was pressed
+    if (e.which === 13) {
+      const term = $('#searchValue').val();
+
+      if (isHash(term)) {
+        const chl = encodeURIComponent(channel.url);
+        return document.location.href=`./transaction.html?channel=${chl}&hash=${term}`;
+      }
+    }
+  });
+});
+
+function fetchChannelStats(clear = false) {
+  if (clear) {
+    clearChannelStats();
+  }
+
+  $.ajax({
+    url: `${channel.url}/api/v1/stats`,
+    dataType: 'json',
+    type: 'GET',
+    cache: 'false',
+    success: function (stats) {
+      $('#channelName').text(stats.channel_name);
+      $('#channelDescription').text(stats.channel_description);
+      $('#channelVersion').text(stats.version);
+      $('#channelContact').text(stats.channel_contact);
+      $('#channelPubKey').text(stats.pub_key_string);
+      $('#channelTxCount').text(stats.tx_objects_on_disk);
+      $('#channelUsersCount').text(stats.tx_graph_users);
+    },
+    error: function() {
+      console.log('error fetching stats!');
+      clearChannelStats();
+    }
+  });
+}
+
+function clearChannelStats() {
+  $('#channelName').text('');
+  $('#channelDescription').text('');
+  $('#channelVersion').text('');
+  $('#channelContact').text('');
+  $('#channelPubKey').text('');
+  $('#channelTxCount').text('');
+  $('#channelUsersCount').text('');
+}
+
+function initChannelSelector() {
+  $('#select-daedalus').click(function() {
+    if (channel.url !== channels.daedalus.url)
+      switchChannel(channels.daedalus);
+  });
+  $('#select-icarus').click(function() {
+    if (channel.url !== channels.icarus.url)
+      switchChannel(channels.icarus);
+  });
+}
+
+function switchChannel(newChan) {
+  channel = newChan;
+  $('#chan-select-title').text(channel.name);
+  fetchChannelStats(true);
+  fetchTransactions(true);
+}
+
+function fetchTransactions(clear = false) {
+  if (clear) {
+    transactionsTable.clear();
+    transactionsTable.draw(false);
+  }
+
+  console.log(`refresh data for channel: ${JSON.stringify(channel)}`);
+
+  $.ajax({
+    url: `${channel.url}/api/v1/transactions/20`,
+    dataType: 'json',
+    type: 'GET',
+    cache: 'false',
+    success: function (txs) {
+      updateTransactionsTable(txs);
+    },
+    error: function() {
+      console.log('error fetching txs!');
+      transactionsTable.clear();
+      transactionsTable.draw(false);
+    }
+  });
+}
+
+function startRefreshTimer(interval) {
+  function refreshData() {
+    setTimeout(function () {
+      fetchTransactions();
+      refreshData();
+    }, interval)
+  }
+  refreshData();
+}
+
+function initTransactionsTable() {
   transactionsTable = $('#transactions').DataTable({
     columnDefs: [{
       targets: [0, 1, 2],
@@ -33,7 +151,7 @@ $(document).ready(function() {
       targets: 2,
       render: function (data, type, row, meta) {
         if (type === 'display') {
-          const chl = encodeURIComponent(channel);
+          const chl = encodeURIComponent(channel.url);
           data = `<a href="./transaction.html?channel=${chl}&hash=${data}">${data}</a>`
         }
         return data;
@@ -51,84 +169,6 @@ $(document).ready(function() {
     },
     autoWidth: false
   }).columns.adjust().responsive.recalc();
-
-  startRefreshTimer(10000);
-
-  // $('#searchButton').click(function () {
-  //   channel = $('#searchValue').val();
-  // });
-
-  $('#searchValue').keydown(function (e) {
-    // setSearchValueErrorState(false);
-
-    // check if 'Enter' key was pressed
-    if (e.which === 13) {
-      const term = $('#searchValue').val();
-
-      if (isHash(term)) {
-        const chl = encodeURIComponent(channel);
-        return document.location.href=`./transaction.html?channel=${chl}&hash=${term}`;
-      }
-    }
-  });
-});
-
-function fetchChannelStats() {
-  $.ajax({
-    url: `${channel}/api/v1/stats`,
-    dataType: 'json',
-    type: 'GET',
-    cache: 'false',
-    success: function (stats) {
-      console.log(stats);
-
-      $('#channelName').text(stats.channel_name);
-      $('#channelDescription').text(stats.channel_description);
-      $('#channelVersion').text(stats.version);
-      $('#channelContact').text(stats.channel_contact);
-      $('#channelPubKey').text(stats.pub_key_string);
-      $('#channelTxCount').text(stats.tx_objects_on_disk);
-      $('#channelUsersCount').text(stats.tx_graph_users);
-    },
-    error: function() {
-      console.log('error fetching stats!');
-      // channel = undefined;
-    }
-  });
-}
-
-function fetchTransactions() {
-  if (!channel || channel === '') {
-    transactionsTable.clear();
-    transactionsTable.draw(false);
-    return;
-  }
-
-  console.log(`refresh data for channel: ${channel}`);
-
-  $.ajax({
-    url: `${channel}/api/v1/transactions/20`,
-    dataType: 'json',
-    type: 'GET',
-    cache: 'false',
-    success: function (txs) {
-      updateTransactionsTable(txs);
-    },
-    error: function() {
-      console.log('error fetching txs!');
-      // channel = undefined;
-    }
-  });
-}
-
-function startRefreshTimer(interval) {
-  function refreshData() {
-    setTimeout(function () {
-      fetchTransactions();
-      refreshData();
-    }, interval)
-  }
-  refreshData();
 }
 
 function updateTransactionsTable(txs) {
