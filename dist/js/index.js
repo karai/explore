fc93dcb9a4fa9a597e296b2c433c8428dc387f67
Daniel Leedan <d4n13l.l33d4n@gmail.com>
2020-10-04 14:51:11 +0800

    Add tooltip for full hash

--- dist/js/index.js
+++ dist/js/index.js
@@ -199,7 +199,7 @@ function initTransactionsTable() {
       render: function (data, type, row, meta) {
         if (type === 'display') {
           const chl = encodeURIComponent(channel.url);
-          data = `<a href="./transaction.html?channel=${chl}&hash=${data}"><span class="transaction-hash">${getHashSegments(data)}</span></a>`;
+          data = `<a href="./transaction.html?channel=${chl}&hash=${data}" data-toggle="tooltip" data-placement="top" title="${data}"><span class="transaction-hash">${getHashSegments(data)}</span></a>`;
         }
         return data;
       }
